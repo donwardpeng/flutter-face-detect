e0b606d4c2d7fe78de167e37af133163e5ceb843
donwardpeng <don.ward.peng@gmail.com>
2019-10-20 21:08:16 -0400

    Followed a tutorial to add face detect to my Flutter app. Pretty simple to follow.

--- lib/ui/face_detect_camera.dart
+++ lib/ui/face_detect_camera.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 class FaceDetectScreen extends StatefulWidget {
   @override
@@ -9,19 +13,46 @@ class FaceDetectScreen extends StatefulWidget {
 }
 
 class FaceDetectStateWidget extends State<FaceDetectScreen> {
-  
-  
-  
+  File _imageFile;
+  List<Face> _faces;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(
-      title: Text(
-        'Face Detector',
-      
-      ),
-    ),
-    body:Column(children:)
-    );
+          title: Text(
+            'Face Detector',
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Detect'),
+              onPressed: () {
+                detectFaces();
+              },
+            )
+          ],
+        ));
+  }
+
+  Future detectFaces() async {
+    final File imageFile =
+        await ImagePicker.pickImage(source: ImageSource.gallery);
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final faceDetector = FirebaseVision.instance.faceDetector(
+        FaceDetectorOptions(
+            mode: FaceDetectorMode.fast,
+            enableLandmarks: true,
+            enableClassification: false,
+            enableTracking: true));
+    final faces = await faceDetector.detectInImage(image);
+
+    if (mounted) {
+      setState(() {
+        _imageFile = imageFile;
+        _faces = faces;
+      });
+    }
   }
 }
